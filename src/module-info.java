d2d87120688a49379d8ad0ead46886226083ccd4
Andrei Malosti <andrei.malosti06@gmail.com>
2024-03-06 11:01:54 -0300

    DepartmentService

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module workshopjavafxjdbc {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires javafx.graphics;
-	
-	opens application to javafx.graphics, javafx.fxml;
-	exports gui to javafx.fxml;     
-	opens gui to javafx.fxml; 
-}
