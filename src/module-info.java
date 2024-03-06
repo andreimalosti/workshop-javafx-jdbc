5fd1a65632c178c922fc5886ef2e0e3854d37715
Andrei Malosti <andrei.malosti06@gmail.com>
2024-03-06 09:49:07 -0300

    About view

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,9 @@
 module workshopjavafxjdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.graphics;
 	
 	opens application to javafx.graphics, javafx.fxml;
+	exports gui to javafx.fxml;     
+	opens gui to javafx.fxml; 
 }
